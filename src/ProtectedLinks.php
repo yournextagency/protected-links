e5fca66e62d5964bf143edf3ebe162273f6b2787
Andrew Mikhailiuk <a@paparoot.ru>
2025-11-25 11:00:55 +0300

    Rename Element class constant
    https://craftcms.com/docs/5.x/extend/updating-plugins.html#element-indexes

--- src/ProtectedLinks.php
+++ src/ProtectedLinks.php
@@ -155,7 +155,7 @@ class ProtectedLinks extends Plugin
 
         Event::on(
             Asset::class,
-            Element::EVENT_SET_TABLE_ATTRIBUTE_HTML,
+            Element::EVENT_DEFINE_ATTRIBUTE_HTML,
             function(craft\events\SetElementTableAttributeHtmlEvent $e) {
                 if($e->attribute === 'downloads'){
                     $e->html = $this->link->downloads($e->sender->id);
